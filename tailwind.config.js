d9dc68fb522c87615bfbf338ed8a6c8c162b0628
alip <alifrizkipambudi@gmail.com>
2021-04-25 18:24:16 +0700

    bismillah

---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: [
+    './public/**/*.html'
+    // './pages/profile-card-component.html'
+  ],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      backgroundImage: theme => ({
+        'intro-mobile': "url('/images/intro-component-with-signup/bg-intro-mobile.png')",
+        'intro-desktop': "url('/images/intro-component-with-signup/bg-intro-desktop.png')",
+        'apparel-pattern-desktop': "url('/images/base-apparel-coming-soon/bg-pattern-desktop.svg')"
+      }),
+      fontFamily: () => ({
+        'poppins': "Poppins",
+        'lexend-deca': "Lexend Deca",
+        'big-shoulders': "Big Shoulders Display",
+        'kumbh': "Kumbh Sans",
+        'spartan': "Spartan",
+        'josefin': "Josefin Sans"
+      })
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
